6837d465c102f3c240bc0fa4ef79e1b0961c2de1
Arthur Rutishauser <arthur.rutishauser@sonntagszeitung.ch>
2015-02-18 08:48:01 +0100

    return

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, 
             :style => { :medium => "300x300>", :thumb => "100x100>" },
-            :default_url => "https://rutishauser.amazons3.com/:style/missing.png"
             :storage => :s3,
             :bucket  => ENV['rutishauser']
 	validates :image, presence: true
